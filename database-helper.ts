d622087ccd78d6e9e09b687346a035e27966cdb3
Djboy08 <justindoane@gmail.com>
2026-01-25 22:51:07 -0500

    fixes

--- database-helper.ts
+++ database-helper.ts
@@ -1,10 +1,7 @@
-export function getBan(db: any, userId: string) {
-  return new Promise(async (resolve, reject) => {
-    const collection = db.collection("Bans");
-    let g = await collection.findOne({
-      UserID: userId,
-    });
-    console.log(g);
-    resolve(g);
+export async function getBan(db: any, userId: string) {
+  const collection = db.collection("Bans");
+  let result = await collection.find({
+    UserID: userId.toString(),
   });
+  return result;
 }
