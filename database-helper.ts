438ebe66977cbbfbe2bb23b5d50398d31f911e8d
Djboy08 <justindoane@gmail.com>
2026-01-25 22:42:29 -0500

    fix

--- database-helper.ts
+++ database-helper.ts
@@ -1,11 +1,13 @@
 export function getBan(db: any, userId: string) {
   return new Promise((resolve, reject) => {
     const collection = db.collection("Bans");
+    console.log("Searching for ban with UserID:", userId);
     collection
       .find({
         UserID: userId,
       })
       .toArray((err: any, docs: any) => {
+        console.log(docs);
         resolve(docs);
       });
   });
