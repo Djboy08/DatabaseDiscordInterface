2f4e6fd7de3c01dda9d97eb6d8436a563ac1951c
Djboy08 <justindoane@gmail.com>
2026-01-25 22:52:32 -0500

    Find One

--- database-helper.ts
+++ database-helper.ts
@@ -1,6 +1,6 @@
 export async function getBan(db: any, userId: string) {
   const collection = db.collection("Bans");
-  let result = await collection.find({
+  let result = await collection.findOne({
     UserID: userId.toString(),
   });
   return result;
