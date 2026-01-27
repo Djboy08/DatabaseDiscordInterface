f65056524b7b7f6e387ae8474e3b228829b33b3c
Djboy08 <justindoane@gmail.com>
2026-01-27 02:34:32 -0500

    Subdomain

--- index.ts
+++ index.ts
@@ -90,20 +90,34 @@ setInterval(async () => {
 
 const server = Bun.serve({
   // `routes` requires Bun v1.2.3+
-  routes: {
-    // Static routes
-    "/api/bansv4": (req) => {
+  //   routes: {
+  //     // Static routes
+  //     "/api/bansv4": (req) => {
+  //       if (BANS) {
+  //         return Response.json(BANS);
+  //       } else {
+  //         return Response.json({ success: false });
+  //       }
+  //     },
+  //   },
+
+  // (optional) fallback for unmatched routes:
+  // Required if Bun's version < 1.2.3
+  fetch(req) {
+    const host = req.headers.get("host");
+    // e.g. "api.example.com:3000"
+
+    const hostname = host?.split(":")[0]; // remove port
+    const subdomain = hostname?.split(".")[0];
+
+    if (subdomain === "api" && req.url.endsWith("/api/bansv4")) {
       if (BANS) {
         return Response.json(BANS);
       } else {
         return Response.json({ success: false });
       }
-    },
-  },
+    }
 
-  // (optional) fallback for unmatched routes:
-  // Required if Bun's version < 1.2.3
-  fetch(req) {
     return new Response("Not Found", { status: 404 });
   },
 });
